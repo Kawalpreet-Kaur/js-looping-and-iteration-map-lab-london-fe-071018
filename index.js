c57a9ca3fdb1bbe0e3730a8cbf49a5e19ea3322e
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-16 19:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // const driver=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
 function lowerCaseDrivers(){
-  return drivers.map(drivers=>drivers.toLowerCase())
+  return drivers.map(lowerCaseDrivers=>drivers.toLowerCase())
 }
 
 

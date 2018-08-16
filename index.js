c5561dbda3b142ee34e67fde14e08ccce0255f4f
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-16 19:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 
-// const driver=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'
+const driver=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
 function lowerCaseDrivers(){
   return driver.map(driver=>driver.toLowerCase())
